c3d23834266c06e63556de4ffa48b1e15258a5a2
millerzhenya69-art <millerzhenya69@gmail.com>
2026-05-21 14:34:57 +0300

    Update ai_clients.py

--- ai_clients.py
+++ ai_clients.py
@@ -1,7 +1,5 @@
 import sys
 import io
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
-sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding="utf-8")
 import os
 import random
 import re
